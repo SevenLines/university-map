146dccf08bc03a9dbb1bcd2211edc6220297a804
m <mmailm@mail.ru>
2019-05-29 11:10:54 +0800

    process

--- client/src/utils/opt_svg_loader.js
+++ client/src/utils/opt_svg_loader.js
@@ -1,4 +1,20 @@
+const {compileTemplate} = require('@vue/component-compiler-utils')
+
 module.exports = function optSvgLoader(source) {
   let r = source.replace(/(rect|path) id="aud-/, 'auditory id="aud-');
-  return r
+
+  // const compiler = require('vue-template-compiler')
+  //
+  // const compiled = compileTemplate({
+  //   source: r,
+  //   compiler,
+  //   filename: this.resourcePath,
+  //   prettify: false,
+  // })
+  // const {code} = compiled
+  //
+  // // finish with ESM exports
+  // let out = code + `\nexport { render, staticRenderFns }`
+  // console.log(r)
+  return r;
 }
--- client/vue.config.js
+++ client/vue.config.js
@@ -4,7 +4,8 @@ module.exports = {
   chainWebpack: (config) => {
     config.module.rule('opt.svg').uses.clear().end()
       .test(/\.opt\.svg$/)
-      .use('opt-svg-loader').loader(path.resolve(__dirname, 'src/utils/opt_svg_loader.js')).end()
+        .use("vue-loader").loader("vue-loader").end()
+        .use('opt-svg-loader').loader(path.resolve(__dirname, 'src/utils/opt_svg_loader.js')).end()
   },
   devServer: {
     proxy: 'http://localhost:8000',
