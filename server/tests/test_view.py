93d5ff93a46d1a586e222124580dfbe20c52eaba
Lydia <lvahr@yandex.ru>
2019-07-04 13:42:34 +0800

    ViewTeacher попытка доделать тест

--- server/tests/test_view.py
+++ server/tests/test_view.py
@@ -1,7 +1,15 @@
+from namespaces.teachers import TeacherWayView
 from tests import TestCaseBase
 
 
 class TestTeacherView(TestCaseBase):
     def test_get_view(self):
-        r = self.api('get', '/api/teachers/WayView?id=3299&day=1')
-        print(r)
+        r = self.api('get', '/api/teachers/way_view?id=3299&day=1')
+        aud = r[0]['auditory']
+        print(aud)
+
+
+
+
+
+
