5bfdb4f8b69bc8547344b3930cd6ac1ef1aa9f16
Lydia <lvahr@yandex.ru>
2019-07-04 14:58:34 +0800

    ViewTeacher. Готовый тест, проверяющий рассписание конкретного дня по id преподавателя путем соответствия ожидаемых данных (номер пары, название аудитории) и выводящихся по запросу из БД

--- server/tests/test_view.py
+++ server/tests/test_view.py
@@ -1,12 +1,20 @@
-from namespaces.teachers import TeacherWayView
 from tests import TestCaseBase
-
 
 class TestTeacherView(TestCaseBase):
     def test_get_view(self):
-        r = self.api('get', '/api/teachers/way_view?id=3299&day=1')
-        aud = r[0]['auditory']
-        print(aud)
+        r = self.api('get', '/api/teachers/way_view?id=3095&day=1')
+        self.assertEqual(len(r), 3)
+        self.assertEqual(r[0]['auditory'], 'В-310а')
+        self.assertEqual(r[1]['auditory'], 'В-105')
+        self.assertEqual(r[2]['auditory'], 'В-105')
+        self.assertEqual(r[0]['para'], 3)
+        self.assertEqual(r[0]['para'], 4)
+        self.assertEqual(r[0]['para'], 5)
+
+
+
+
+
 
 
 
