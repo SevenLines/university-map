007fb8e276dad66921426ae9b15aa711ea3880aa
isachenkoff.alexander <isachenkoff.alexander@yandex.ru>
2019-07-04 19:15:59 +0800

    • Добавил соединение узлов на перерекёстках в один узел при чтении графа.  • Исправил некоторые id в svg.  • Тест чтения графа из svg: кол-во рёбер == кол-во узлов - 1 (пока граф дерево).  • Запись графа в файл: функция write_graph(graph, path).  • Чтение графа из файла: функция read_graph(path).  • Тестирование записи и чтения: проверка эквивалентности всех элементов графа.  • Поиск пути из одного узла графа в другой по индексу в списке узлов (nodes).  • Метод для поиска индекса узла index_of_node(node) вызывается из графа.  • Метод для поиска индекса узла по id index_by_id(id) вызывается из графа.  • Тест: выводит id узлов пути из Г-303 в В-316  • Тест: расставляет в svg точки узлов пути из Г-303 в В-316 (файл path_nodes.svg)

--- server/tests/test_ways.py
+++ server/tests/test_ways.py
@@ -1,12 +1,47 @@
-from ways import get_graph
+from ways import get_from_svg, write_graph, read_graph, find_paths
 from unittest import TestCase, main
 
 
 class TestGraphReading(TestCase):
+
+    def setUp(self):
+        self.svg_path = '../../Data/3этаж.svg'
+        self.save_path = 'graph.bin'
+
     def test_get_graph(self):
-        graph = get_graph("../../Data/3этаж.svg")
-        self.assertTrue(len(graph.nodes) > 100)
-        self.assertTrue(len(graph.edges) > 100)
+        graph = get_from_svg(self.svg_path)
+        self.assertEqual(len(graph.edges), len(graph.nodes) - 1)
+
+    def test_read_graph(self):
+        graph = get_from_svg(self.svg_path)
+        write_graph(graph, self.save_path)
+        new_graph = read_graph(self.save_path)
+        assert new_graph == graph
+
+    def test_find_path(self):
+        graph = get_from_svg(self.svg_path)
+        for list in find_paths(graph.rel_list, graph.index_by_id('enter_g303'), graph.index_by_id('enter_v316')):
+            for i in list:
+                print(graph.nodes[i].id)
+
+    def test_find_path_with_view(self):
+        graph = get_from_svg(self.svg_path)
+        lines = []
+        f = open(self.svg_path)
+        for line in f.readlines():
+            lines.append(line)
+        f.close()
+        nodes_code = ''
+        for list in find_paths(graph.rel_list, graph.index_by_id('enter_g303'), graph.index_by_id('enter_v316')):
+            for i in list:
+                nodes_code += '<circle\nstyle=\"display:inline;fill:#3333cc;fill-opacity:1;stroke:none;stroke-width:0' \
+                              '.1\"\nr=\"0.4\"\ncy=\"' + str(graph.nodes[i].point.y) + \
+                              '\"\ncx=\"' + str(graph.nodes[i].point.x) + '\"\nid=\"' + str(graph.nodes[i].id) + '\" />\n'
+        lines.insert(len(lines)-2, nodes_code)
+        f = open('path_nodes.svg', 'w')
+        for line in lines:
+            f.write(line)
+        f.close()
 
 
 if __name__ == '__main__':
--- server/ways.py
+++ server/ways.py
@@ -1,49 +1,77 @@
 from xml.dom.minidom import parseString
+import pickle
 import math
 
 
 class Point(object):
-    x = 0
-    y = 0
-
     def __init__(self, x, y):
         self.x = float(x)
         self.y = float(y)
 
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
 
 class Node(object):
-    id = ''
-    point = None
-
     def __init__(self, id, point):
         self.id = id
         self.point = point
 
+    def __eq__(self, other):
+        return self.point == other.point and self.id == other.id
+
 
 class Edge(object):
-    first = None
-    second = None
-
     def __init__(self, first, second):
         self.first = first
         self.second = second
 
+    def __eq__(self, other):
+        return self.first == other.first and self.second == other.second
+
 
 class Graph(object):
-    nodes = []
-    edges = []
-
     def __init__(self, nodes, edges):
         self.nodes = nodes
         self.edges = edges
+        self.rel_list = [[]]
+        for i in range(len(self.nodes)):
+            if i > 0:
+                self.rel_list.append([])
+            for j in range(len(self.edges)):
+                if self.nodes[i] == self.edges[j].first:
+                    self.rel_list[i].append(self.index_of_node(self.edges[j].second))
+                elif self.nodes[i] == self.edges[j].second:
+                    self.rel_list[i].append(self.index_of_node(self.edges[j].first))
+
+    def __eq__(self, other):
+        if len(self.nodes) != len(other.nodes):
+            return False
+        if len(self.edges) != len(other.edges):
+            return False
+        for i in range(len(self.nodes)):
+            if self.nodes[i] != other.nodes[i]:
+                return False
+        for i in range(len(self.edges)):
+            if self.edges[i] != other.edges[i]:
+                return False
+        return True
+
+    def index_of_node(self, node):
+        for i in range(len(self.nodes)):
+            if self.nodes[i] == node:
+                return i
+
+    def index_by_id(self, id):
+        for i in range(len(self.nodes)):
+            if self.nodes[i].id == id:
+                return i
 
 
-# Возвращает граф из .svg по адресу 'path'
-def get_graph(path):
+def get_from_svg(path) -> Graph:
     file = open(path)
     svg = parseString(file.read())
     file.close()
-    nodes = []
     edges = []
     auto_id = 0
 
@@ -51,6 +79,8 @@ def get_graph(path):
         if g.getAttribute('id') == 'ways_layer':
             ways_layer = g
             break
+
+    nodes = get_vertex(ways_layer)
 
     for path in ways_layer.getElementsByTagName('path'):
         d = path.getAttribute('d')
@@ -86,21 +116,28 @@ def get_graph(path):
         auto_id += 1
         second = Node(auto_id, Point(x2, y2))
         auto_id += 1
-        nodes.append(first)
-        nodes.append(second)
+
+        found = False
+        for node in nodes:
+            if is_near(node.point, first.point):
+                first = node
+                found = True
+                break
+        if not found:
+            nodes.append(first)
+
+        found = False
+        for node in nodes:
+            if is_near(node.point, second.point):
+                second = node
+                found = True
+                break
+        if not found:
+            nodes.append(second)
+
         edges.append(Edge(first, second))
 
-    graph = Graph(nodes, edges)
-
-    vertex = get_vertex(ways_layer)
-
-    for node in graph.nodes:
-        for v in vertex:
-            if is_near(node.point, v.point):
-                node.id = v.id
-                break
-
-    return graph
+    return Graph(nodes, edges)
 
 
 def get_vertex(scope) -> [Node]:
@@ -114,8 +151,32 @@ def get_vertex(scope) -> [Node]:
 
 
 def is_near(first, second):
-    z = (first.x - second.x)**2 + (first.y - second.y)**2
-    if math.sqrt(z) < 1:  # какая погрешность?
+    z = (first.x - second.x) ** 2 + (first.y - second.y) ** 2
+    if math.sqrt(z) < 0.5:
         return True
     else:
         return False
+
+
+def read_graph(path) -> Graph:
+    file = open(path, 'rb')
+    graph = pickle.load(file)
+    file.close()
+    return graph
+
+
+def write_graph(graph, path):
+    file = open(path, 'wb')
+    pickle.dump(graph, file)
+    file.close()
+
+
+def find_paths(graph, start, goal):
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        for next in set(graph[vertex]) - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                stack.append((next, path + [next]))
