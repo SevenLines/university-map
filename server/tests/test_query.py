9fa1cde3a85deab18ebec6d6d5b4857184250560
Lydia <lvahr@yandex.ru>
2019-07-04 16:10:16 +0800

    SQL запрос для расчета маршрутов движения без учета заочного расписания конкретной группы на заданную дату

--- server/tests/test_query.py
+++ server/tests/test_query.py
@@ -1,6 +1,7 @@
 import tests
 from sqlalchemy import func
-from models.raspnagr import Auditory, Raspis, Raspnagr, Teacher
+from sqlalchemy.sql.functions import coalesce
+from models.raspnagr import Auditory, Raspis, Raspnagr, Teacher, Kontgrp, Kontkurs, Potoklist
 
 
 class TestQuery(tests.TestCaseBase):
@@ -31,3 +32,34 @@ class TestQuery(tests.TestCaseBase):
 
             for item in schedule:
                 print(f"Пара: {item.para}  Аудитория: {item.auditory}")
+
+        def test_filter_by_group_and_day(self):
+            """
+            SELECT para, a.obozn as aud,coalesce(pl.konts, kg.obozn, kk.obozn)as kont
+            FROM raspis r
+            LEFT JOIN auditories a ON r.aud = a.id_60
+            LEFT JOIN raspnagr rn ON rn.id_51 = r.raspnagr
+            LEFT JOIN kontgrp kg ON kg.id_7 = rn.kontid
+            LEFT JOIN kontkurs kk ON kk.id_1 = rn.kont
+            LEFT JOIN potoklist pl ON pl.op = rn.op
+            WHERE (day-1)%7+1 = 1 AND kg.kont = 22979
+            ORDER BY para
+         """
+        schedule = Raspis.query \
+        .filter((Raspis.day -1)%7 ==1)\
+        .filter(Kontgrp.kont_id == 22979)\
+        .outerjoin(Auditory, Raspis.aud_id == Auditory.id)\
+        .outerjoin(Raspnagr, Raspnagr.id == Raspis.raspnagr_id)\
+        .outerjoin(Kontgrp, Kontgrp.id == Raspnagr.kontgrp_id) \
+        .outerjoin(Kontkurs, Kontkurs.id == Raspnagr.kontkurs_id) \
+        .outerjoin(Potoklist, Potoklist.op == Raspnagr.op)\
+        .with_entities(
+        Raspis.para,
+        func.rtrim(Auditory.title).label("auditory"),
+        coalesce(Potoklist.title, Kontgrp.title, Kontkurs.title).label("group")
+        ) \
+        .order_by(Raspis.para)
+        print(schedule)
+
+        for item in schedule:
+            print(f"Пара: {item.para} Аудитория: {item.auditory} Группа: {item.group}")
